6f654f09f01ec6c7c1519bcc7b7133d5736154cd
Artem Naumov <arttime32@gmail.com>
2022-11-20 12:12:59 +0200

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,36 @@
+import { Module } from '@nestjs/common';
+import { PostsModule } from './posts/posts.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
+import { DatabaseModule } from './database/database.module';
+import { AuthenticationModule } from './authentication/authentication.module';
+import { APP_FILTER } from '@nestjs/core';
+import { ExceptionsLoggerFilter } from './utils/exceptionsLogger.filter';
+
+@Module({
+  imports: [
+    PostsModule,
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        PORT: Joi.number(),
+        JWT_SECRET: Joi.string().required(),
+        JWT_EXPIRATION_TIME: Joi.string().required(),
+      }),
+    }),
+    DatabaseModule,
+    AuthenticationModule,
+  ],
+  controllers: [],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: ExceptionsLoggerFilter,
+    },
+  ],
+})
+export class AppModule {}
---
+++ src/posts/posts.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@nestjs/common';
+import CreatePostDto from './dto/createPost.dto';
+import { UpdatePostDto } from './dto/updatePost.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import PostEntity from './post.entity';
+import { PostNotFoundException } from './exception/postNotFund.exception';
+
+@Injectable()
+export default class PostsService {
+  constructor(
+    @InjectRepository(PostEntity)
+    private postsRepository: Repository<PostEntity>,
+  ) {}
+
+  getAllPosts() {
+    return this.postsRepository.find();
+  }
+
+  async getPostById(id: number) {
+    const post = await this.postsRepository.findOne({ where: { id } });
+    if (post) {
+      return post;
+    }
+    throw new PostNotFoundException(id);
+  }
+
+  async updatePost(id: number, post: UpdatePostDto) {
+    await this.postsRepository.update(id, post);
+    const updatedPost = await this.postsRepository.findOne({ where: { id } });
+    if (updatedPost) {
+      return updatedPost;
+    }
+    throw new PostNotFoundException(id);
+  }
+
+  async createPost(post: CreatePostDto) {
+    const newPost = await this.postsRepository.create(post);
+    await this.postsRepository.save(newPost);
+    return newPost;
+  }
+
+  async deletePost(id: number) {
+    const deleteResponse = await this.postsRepository.delete(id);
+    if (!deleteResponse.affected) {
+      throw new PostNotFoundException(id);
+    }
+  }
+}
